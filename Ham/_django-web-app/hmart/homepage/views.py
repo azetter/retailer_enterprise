66dd759d8068602f354353a679f7a575f1187996
Hammania689 <hameedabdulrashid@gmail.com>
2018-04-16 00:40:08 -0500

    Change to path

--- Ham/_django-web-app/hmart/homepage/views.py
+++ Ham/_django-web-app/hmart/homepage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('안녕하세요 하마즈입니다')
+    return HttpResponse('안녕하세요 한아름 마트십니다')

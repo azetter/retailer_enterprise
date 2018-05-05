534ba757cc8bf5cc921b16a0d9b3ab19a545c16a
Hammania689 <hameedabdulrashid@gmail.com>
2018-05-05 10:48:43 -0500

    It's been a minute

--- Ham/_django-web-app/hmart/homepage/urls.py
+++ Ham/_django-web-app/hmart/homepage/urls.py
@@ -1,6 +1,24 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .import views
+from homepage.models import *
+from homepage.views import StoreList, ProductList, SalesList
 
 urlpatterns = [
+    # Index page
     path('', views.index, name ='index'),
-]
+
+    # Stores/
+    path('stores/', StoreList.as_view()),
+
+    # Products/
+    path('products/', ProductList.as_view()),
+
+    # Sales/
+    # path('sales/', views.queryTest, name='sales'),
+    path('sales/', SalesList.as_view()),
+
+     # Sales/
+    # path('storesales/', views.queryTest1, name='store'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
